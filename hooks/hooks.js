5bef6652fb3f0e526db9cac04e70d66e69dd8cd7
s4-islam <s4.islam@a1qa.com>
2026-05-21 17:37:09 +0600

    Initial Setup for project

---
+++ hooks/hooks.js
@@ -0,0 +1,18 @@
+const { After } = require('@cucumber/cucumber');
+
+After(async function (scenario) {
+
+    if (scenario.result.status === 'FAILED') {
+
+        const screenshot = await browser.takeScreenshot();
+
+        await this.attach(
+            Buffer.from(screenshot, 'base64'),
+            'image/png'
+        );
+
+        await browser.saveScreenshot(
+            `./screenshots/${scenario.pickle.name}.png`
+        );
+    }
+});
