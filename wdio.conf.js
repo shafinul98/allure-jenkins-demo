4d20e62972ee772f4ce4c1268ae316bb7f15a5e3
sislamLLI <Shafinul.Islam@lloydslistintelligence.com>
2026-06-08 17:51:07 +0600

    Added chromedriver package and updated the wdio.conf.js file

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,6 +51,9 @@ export const config = {
     //
     capabilities: [{
         browserName: 'chrome',
+        'wdio:chromedriverOptions': {
+            binary: require('chromedriver').path, 
+        }
         'goog:chromeOptions': {
             args: ['--headless', '--disable-gpu', '--window-size=1920,1080']
         }
